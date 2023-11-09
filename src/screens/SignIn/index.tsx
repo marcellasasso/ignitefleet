a0e1a99c1c615c96628f890d4988d2be9070b7b8
marcellasasso <marcellasasso@gmail.com>
2023-11-09 12:01:42 -0300

    até aula habilitando network access

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,13 +1,62 @@
-import backgroundImg from '../../assets/background.png'
+import { useEffect, useState } from 'react'
+import { Alert } from 'react-native'
+
+import * as WebBrowser from 'expo-web-browser'
+import * as Google from 'expo-auth-session/providers/google'
 
 import { Container, Slogan, Title } from './styles'
 
+import { ANDROID_CLIENT_ID, IOS_CLIENT_ID } from '@env'
+
+import backgroundImg from '../../assets/background.png'
+
+import { Button } from '../../components/Button'
+
+WebBrowser.maybeCompleteAuthSession()
+
 export function SignIn() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false)
+
+  const [_, response, googleSignIn] = Google.useAuthRequest({
+    androidClientId: ANDROID_CLIENT_ID,
+    iosClientId: IOS_CLIENT_ID,
+    scopes: ['profile', 'email'],
+  })
+
+  function handleGoogleSignIn() {
+    setIsAuthenticating(true)
+
+    googleSignIn().then((response) => {
+      if (response.type !== 'success') {
+        setIsAuthenticating(false)
+      }
+    })
+  }
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      if (response.authentication?.idToken) {
+      } else {
+        Alert.alert(
+          'Entrar',
+          'Não foi possível conectar-se a sua conta Google.',
+        )
+        setIsAuthenticating(false)
+      }
+    }
+  }, [response])
+
   return (
     <Container source={backgroundImg}>
       <Title>Ignite Fleet</Title>
 
       <Slogan>Gestão de uso de veículos</Slogan>
+
+      <Button
+        title="Entrar com Google"
+        onPress={handleGoogleSignIn}
+        isLoading={isAuthenticating}
+      />
     </Container>
   )
 }
